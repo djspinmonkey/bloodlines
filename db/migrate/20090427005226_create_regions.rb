a8b5850f1c148bdd5e0571c62e1c3b9d6bcca498
John Hyland <github@djspinmonkey.com>
2009-04-26 21:04:29 -0400

    added region scaffolding

---
+++ db/migrate/20090427005226_create_regions.rb
@@ -0,0 +1,14 @@
+class CreateRegions < ActiveRecord::Migration
+  def self.up
+    create_table :regions do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :regions
+  end
+end
