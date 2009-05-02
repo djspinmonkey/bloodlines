aebc264136ccdff8cc5de29f2ba25e7ea48bb742
John Hyland <github@djspinmonkey.com>
2009-05-02 17:03:06 -0400

    added some tests

--- test/unit/marriage_test.rb
+++ test/unit/marriage_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class MarriageTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def test_teh_gay
+    adam  = characters(:dude1)
+    steve = characters(:dude2)
+    first_dude_id = [adam.id, steve.id].min
+
+    m = Marriage.new
+    m.characters = [adam, steve]
+
+    assert_equal 2, m.characters.size, "Wrong number of members found in gay marriage"
+    assert_nil m.wife_id
+    assert_equal first_dude_id, m.husband_id, "Found wrong husband id for gay marriage (which is maybe ambiguous anyway)"
   end
+
 end
